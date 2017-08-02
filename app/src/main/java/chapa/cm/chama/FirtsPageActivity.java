f6c23fab8db6880941b169db3d8561465ec7e0b6
toni <antonyngethe@gmail.com>
2017-08-02 06:12:13 +0300

    LATEST

--- app/src/main/java/chapa/cm/chama/FirtsPageActivity.java
+++ app/src/main/java/chapa/cm/chama/FirtsPageActivity.java
@@ -19,7 +19,7 @@ public class FirtsPageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_firts_page);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+getSupportActionBar().setTitle("CHW");
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
